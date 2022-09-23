29c792405edd98379995c1475d2517f646084fa9
kudretyilmaz <kudret.yilmaz@id3.com.tr>
2022-09-23 23:58:12 +0300

    remove language icon animation

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -19,11 +19,8 @@ import { menu } from "./menu";
 // Import Dark Mode Switch
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 
-// Import Framer Motion
-import { motion } from "framer-motion";
-
-import TurkishFlag from "./TurkishFlag";
-import EnglishFlag from "./EnglishFlag";
+import TurkishFlag from "./_partials/TurkishFlag";
+import EnglishFlag from "./_partials/EnglishFlag";
 
 const Header: NextComponentType = () => {
 	// Variables
@@ -44,21 +41,7 @@ const Header: NextComponentType = () => {
 		<header className="flex items-center justify-between h-20 max-w-[1200px] mx-auto w-full px-3">
 			<div className="flex items-center ">
 				<div className="mr-4 transition" onClick={themeSwitcher}>
-					<motion.div
-						className="w-full"
-						variants={{
-							initial: { opacity: 0 },
-							animate: { opacity: 1 },
-							exit: { opacity: 0 },
-						}}
-						initial="initial"
-						animate="animate"
-						exit="exit"
-						key={router.locale}
-						transition={{ duration: 1 }}
-					>
-						{router.locale === "tr" ? <TurkishFlag /> : <EnglishFlag />}
-					</motion.div>
+					{router.locale === "tr" ? <TurkishFlag /> : <EnglishFlag />}
 				</div>
 
 				<div>
