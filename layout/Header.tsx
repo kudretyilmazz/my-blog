83a575a9501b48c3575c120c9a7a46399f6c4f77
kudretyilmaz <kudret.yilmaz@id3.com.tr>
2022-09-23 23:48:04 +0300

    dark mode & language switcher changed

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -16,8 +16,12 @@ import { useTheme } from "next-themes";
 // Import Menu
 import { menu } from "./menu";
 
-import Moon from "./Moon";
-import Sun from "./Sun";
+// Import Dark Mode Switch
+import { DarkModeSwitch } from "react-toggle-dark-mode";
+
+// Import Framer Motion
+import { motion } from "framer-motion";
+
 import TurkishFlag from "./TurkishFlag";
 import EnglishFlag from "./EnglishFlag";
 
@@ -40,11 +44,29 @@ const Header: NextComponentType = () => {
 		<header className="flex items-center justify-between h-20 max-w-[1200px] mx-auto w-full px-3">
 			<div className="flex items-center ">
 				<div className="mr-4 transition" onClick={themeSwitcher}>
-					{router.locale === "tr" ? <TurkishFlag /> : <EnglishFlag />}
+					<motion.div
+						className="w-full"
+						variants={{
+							initial: { opacity: 0 },
+							animate: { opacity: 1 },
+							exit: { opacity: 0 },
+						}}
+						initial="initial"
+						animate="animate"
+						exit="exit"
+						key={router.locale}
+						transition={{ duration: 1 }}
+					>
+						{router.locale === "tr" ? <TurkishFlag /> : <EnglishFlag />}
+					</motion.div>
 				</div>
 
-				<div onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-					{theme === "dark" ? <Moon /> : <Sun />}
+				<div>
+					<DarkModeSwitch
+						sunColor="#F2AB0C"
+						checked={theme === "dark"}
+						onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+					/>
 				</div>
 			</div>
 			<nav>
