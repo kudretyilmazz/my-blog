4c5238d5cf72f4e9c2bfb5f340b37fc7b9e58c9a
kudret.yilmaz <kudret.yilmaz@id3.com.tr>
2022-10-26 22:16:11 +0300

    mobile menu

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -16,11 +16,16 @@ import { useTheme } from "next-themes";
 // Import Menu
 import { menu } from "./menu";
 
+import { motion, AnimatePresence } from "framer-motion";
 // Import Dark Mode Switch
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 
+// Import Flag Icon
 import TurkishFlag from "./_partials/TurkishFlag";
 import EnglishFlag from "./_partials/EnglishFlag";
+
+// Import Menu Icon
+import { Divide as Hamburger } from "hamburger-react";
 
 const Header: NextComponentType = () => {
 	// Variables
@@ -30,6 +35,7 @@ const Header: NextComponentType = () => {
 
 	// useStates
 	const [activeLang, setActiveLang] = useState(router.locale);
+	const [isMobileOpen, setIsMobileOpen] = useState<boolean>(false);
 
 	// Functions
 	const themeSwitcher = () => {
@@ -40,7 +46,7 @@ const Header: NextComponentType = () => {
 	return (
 		<header className="flex items-center justify-between h-20 max-w-[1200px] mx-auto w-full px-3">
 			<div className="flex items-center ">
-				<div className="mr-4 transition" onClick={themeSwitcher}>
+				<div className="mr-4 ml-3 " onClick={themeSwitcher}>
 					{router.locale === "tr" ? <TurkishFlag /> : <EnglishFlag />}
 				</div>
 
@@ -54,8 +60,42 @@ const Header: NextComponentType = () => {
 					/>
 				</div>
 			</div>
-			<nav>
-				<ul className="flex gap-x-6 md:gap-x-10">
+			<div className="md:hidden transition-all">
+				<div>
+					<Hamburger size={20} toggled={isMobileOpen} toggle={setIsMobileOpen} />
+				</div>
+
+				<nav className={`${isMobileOpen ? "flex" : "hidden"} transition-all`}>
+					<ul
+						className={`flex bg-white w-full  absolute left-0 h-40 z-10 flex-col justify-evenly items-center  `}
+					>
+						{menu?.map((item: any, index) => {
+							return (
+								<li
+									key={index}
+									className="font-bold select-none cursor-pointer flex justify-center items-center w-full h-full border-b-2 last:border-black"
+								>
+									<Link href={item?.url}>
+										<a
+											className={
+												router.asPath.includes(item?.url)
+													? router.asPath !== "/" && item?.title === "HOME"
+														? ""
+														: "text-secondary"
+													: ""
+											}
+										>
+											{t(item?.title)}
+										</a>
+									</Link>
+								</li>
+							);
+						})}
+					</ul>
+				</nav>
+			</div>
+			<nav className="hidden md:flex">
+				<ul className=" md:flex md:gap-x-10">
 					{menu?.map((item: any, index) => {
 						return (
 							<li
